b19bb47ea9cd4c443cdcdab09fd392c409447546
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-05 15:44:30 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,17 @@ st.title("Machine Learning Projects")
 st.header("Classification Machine Learning Project")
 url = "https://avinashc.streamlit.app"
 st.markdown(url)
+
+st.header("Random forest Machine Learning Project")
+url = "https://avinashrf.streamlit.app"
+st.markdown(url)
+
+st.header("Linear Regression Machine Learning Project")
+url = "https://avinashli.streamlit.app"
+st.markdown(url)
+
+st.header("Logistic Regression Machine Learning Project")
+url = "https://avinashlr.streamlit.app"
+st.markdown(url)
+
+st.markdown("All above project developed by Avinash Pawar at NIELIT Daman")
