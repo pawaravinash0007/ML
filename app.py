340bfffc2530b366c2e5a302ce81fe19331e607b
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-05 15:53:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,23 +2,21 @@ import streamlit as st
 
 # Page Title and Header
 st.title("Explore Machine Learning Projects")
-
+st.header("Discover various machine learning projects developed by Avinash Pawar at NIELIT Daman")
 
 # Project Links and Descriptions
 projects = {
-    "This project demonstrates classification algorithms.",
-    "Random forest is a popular ensemble learning algorithm.",
-    "Linear regression is a fundamental algorithm for regression tasks.",
-    "Logistic regression is widely used for binary classification problems."
+    "Classification Machine Learning Project": "Explore a classification machine learning project by Avinash Pawar. This project demonstrates classification algorithms.",
+    "Random Forest Machine Learning Project": "Discover a random forest machine learning project developed by Avinash Pawar. Random forest is a popular ensemble learning algorithm.",
+    "Linear Regression Machine Learning Project": "Check out a linear regression machine learning project by Avinash Pawar. Linear regression is a fundamental algorithm for regression tasks.",
+    "Logistic Regression Machine Learning Project": "Explore a logistic regression machine learning project developed by Avinash Pawar. Logistic regression is widely used for binary classification problems."
 }
 
 # Displaying Projects with Descriptions and Links
-for project in projects.items():
+for project, description in projects.items():
     st.header(project)
-    #st.markdown(description)
+    st.markdown(description)
     st.markdown(f"[Click here to explore {project}](https://avinashc.streamlit.app)")
 
 # Footer
-st.markdown("Discover various machine learning projects developed by Avinash Pawar at NIELIT Daman")
 st.markdown("Feel free to explore these projects and learn more about machine learning techniques!")
-st.markdown("Contact Avinash Pawar: 8329063970")
