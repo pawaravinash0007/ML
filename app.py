51a7680b895a4f1581dd44e8b5077092a75d97d2
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-06 08:27:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,25 +3,32 @@ import streamlit as st
 # Page Title and Header
 st.title("Explore Machine Learning Projects NIELIT Daman")
 
-
 # Project Links and Descriptions
 projects = {
-    "Classification Machine Learning Project": "This project demonstrates classification algorithms.",
-    "Random Forest Machine Learning Project": "Random forest is a popular ensemble learning algorithm.",
-    "Linear Regression Machine Learning Project": "Linear regression is a fundamental algorithm for regression tasks.",
-    "Logistic Regression Machine Learning Project": "Logistic regression is widely used for binary classification problems."
+    "Classification Machine Learning Project": {
+        "description": "This project demonstrates classification algorithms.",
+        "link": "https://avinashc.streamlit.app"
+    },
+    "Random Forest Machine Learning Project": {
+        "description": "Random forest is a popular ensemble learning algorithm.",
+        "link": "https://avinashrf.streamlit.app"
+    },
+    "Linear Regression Machine Learning Project": {
+        "description": "Linear regression is a fundamental algorithm for regression tasks.",
+        "link": "https://avinashli.streamlit.app"
+    },
+    "Logistic Regression Machine Learning Project": {
+        "description": "Logistic regression is widely used for binary classification problems.",
+        "link": "https://avinashlr.streamlit.app"
     }
+}
 
 # Displaying Projects with Descriptions and Links
-for project, description in projects.items():
+for project, details in projects.items():
     st.header(project)
-    st.markdown(description)
-    st.markdown(f"[Click here to explore {project}](https://avinashc.streamlit.app)")
-    st.markdown(f"[Click here to explore {project}](https://avinashrf.streamlit.app)")
-    st.markdown(f"[Click here to explore {project}](https://avinashli.streamlit.app)")
-    st.markdown(f"[Click here to explore {project}](https://avinashlr.streamlit.app)")
+    st.markdown(details["description"])
+    st.markdown(f"Click here to explore {project}")
 
 # Footer
 st.markdown("Discover various machine learning projects developed by Avinash Pawar at NIELIT Daman")
 st.markdown("Feel free to Contact Avinash: 8329063970 for Machine Learning Project ")
-
