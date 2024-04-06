5f9fb5f8adbae02ac016bf014f905b9604b5f141
Avinash Pawar <41367003+pawaravinash0007@users.noreply.github.com>
2024-04-06 08:21:32 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,20 +7,19 @@ st.title("Explore Machine Learning Projects NIELIT Daman")
 # Project Links and Descriptions
 projects = {
     "Classification Machine Learning Project": "This project demonstrates classification algorithms.",
-    st.markdown(f"[Click here to explore {project}](https://avinashc.streamlit.app)"),
     "Random Forest Machine Learning Project": "Random forest is a popular ensemble learning algorithm.",
-    st.markdown(f"[Click here to explore {project}](https://avinashrf.streamlit.app)"),
     "Linear Regression Machine Learning Project": "Linear regression is a fundamental algorithm for regression tasks.",
-    st.markdown(f"[Click here to explore {project}](https://avinashli.streamlit.app)"),
     "Logistic Regression Machine Learning Project": "Logistic regression is widely used for binary classification problems."
-    st.markdown(f"[Click here to explore {project}](https://avinashlr.streamlit.app)"),
-}
+    }
 
 # Displaying Projects with Descriptions and Links
 for project, description in projects.items():
     st.header(project)
     st.markdown(description)
     st.markdown(f"[Click here to explore {project}](https://avinashc.streamlit.app)")
+    st.markdown(f"[Click here to explore {project}](https://avinashrf.streamlit.app)")
+    st.markdown(f"[Click here to explore {project}](https://avinashli.streamlit.app)")
+    st.markdown(f"[Click here to explore {project}](https://avinashlr.streamlit.app)")
 
 # Footer
 st.markdown("Discover various machine learning projects developed by Avinash Pawar at NIELIT Daman")
